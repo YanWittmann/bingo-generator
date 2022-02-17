cf960d31bbc74f3ee57102f40955d9970f9fe4cb
Yan Wittmann <order@yanwittmann.de>
2022-02-17 09:28:53 +0100

    Removed old files

--- web-interface/src/main/website/backend/db-login.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = new mysqli('localhost', '', '', '');
-
-// check if the connection works
-if ($db->connect_error) {
-    die_with_message_and_error("connection with database failed", $db->connect_error);
-}
